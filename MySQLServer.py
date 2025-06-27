bab1877b90ff85409ac7b54b9bacec8b401e417e
tomibzan <thomflowen@gmail.com>
2025-06-27 23:04:39 +0300

    third correction

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,39 +1,30 @@
 import mysql.connector
-from mysql.connector import Error, errorcode
+from mysql.connector import Error
 
 
 def create_database():
     connection = None
     try:
-
         connection = mysql.connector.connect(
             host='localhost',
-            user='your_username',
-            password='your_password'
+            user='root',
+            password='alxbe'
         )
 
         if connection.is_connected():
             cursor = connection.cursor()
-            try:
-                cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-                print("Database 'alx_book_store' created successfully!")
-            except Error as err:
-                print(f"Failed to create database: {err}")
-            finally:
-                if 'cursor' in locals() and cursor:
-                    cursor.close()
 
-    except Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Authentication Error: Check your username/password.")
-        elif err.errno == errorcode.ER_HOST_NOT_PRIVILEGED:
-            print("Host not privileged to connect to MySQL server.")
-        else:
-            print(f"Connection Error: {err}")
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
 
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
     finally:
+        # Close the connection
         if connection and connection.is_connected():
+            cursor.close()
             connection.close()
+            print("MySQL connection is closed")
 
 
 if __name__ == "__main__":
