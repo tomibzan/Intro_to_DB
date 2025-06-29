d89fbbc169e9dedda22b27c70ca0b1fd5b4cb638
tomibzan <thomflowen@gmail.com>
2025-06-29 19:24:43 +0300

    used wrong spacing

--- task_6.sql
+++ task_6.sql
@@ -12,9 +12,9 @@ from mysql.connector import connect,
         VALUES (%s, %s, %s, %s)
         """ # List of customers to insert
     customer_data = [
-            (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
-            (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
-            (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.")
+            (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."),
+            (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."),
+            (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.")
         ] with connection.cursor() as cursor: cursor.executemany(insert_query, customer_data) connection.commit() print(
         f "{cursor.rowcount} customers inserted successfully."
     )
